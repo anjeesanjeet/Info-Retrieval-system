ca74f9bb8d796fbf493471eefcb42d76dbe73079
Sanjeet Kumar <anjeesanjeet@gmail.com>
2025-09-22 01:58:00 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="info_retrieval_system",
+    version="0.0.0",
+    author="Sanjeet Kumar",
+    author_email="anjeesanjeet@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
